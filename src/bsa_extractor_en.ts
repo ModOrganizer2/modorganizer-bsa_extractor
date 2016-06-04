8cd815dd9a25d1879f5c2a57ab36b4876b0a9c08
Tannin <sherb@gmx.net>
2016-06-04 19:50:30 +0200

    translation

---
+++ src/bsa_extractor_en.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>BsaExtractor</name>
+    <message>
+        <location filename="bsaextractor.cpp" line="48"/>
+        <source>Offers a dialog during installation of a mod to unpack all its BSAs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bsaextractor.cpp" line="85"/>
+        <source>invalid mod name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bsaextractor.cpp" line="86"/>
+        <source>BSA extraction doesn&apos;t work on mods that have the same name as a non-MO mod.Please remove the mod then reinstall with a different name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bsaextractor.cpp" line="94"/>
+        <source>Extract BSA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bsaextractor.cpp" line="95"/>
+        <source>This mod contains at least one BSA. Do you want to unpack it?
+(This removes the BSA after completion. If you don&apos;t know about BSAs, just select no)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bsaextractor.cpp" line="103"/>
+        <source>failed to read %1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bsaextractor.cpp" line="117"/>
+        <source>This archive contains invalid hashes. Some files may be broken.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
